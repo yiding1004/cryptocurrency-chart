4dde56680438cf3d7323bb2f7b5bb0beb41410d5
YiDing <yiding@Yi-Dings-MacBook-Pro.local>
2024-02-28 16:03:11 -0800

    initial project and create UI component for MUI chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,163 @@
-import Image from "next/image";
+"use client";
+
 import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
+import {
+  ChartsAxisHighlight,
+  ChartsReferenceLine,
+  ChartsTooltip,
+  ChartsXAxis,
+  ChartsYAxis,
+  LineHighlightPlot,
+  LinePlot,
+  MarkPlot,
+  ResponsiveChartContainer,
+} from "@mui/x-charts";
+import { ChartsLegend } from "@mui/x-charts/ChartsLegend";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const inputData = {
+  json: {
+    tokens: [
+      "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9",
+      "untrn",
+    ],
+    chainId: "neutron-1",
+    dateRange: "D7",
+  },
+};
+const jsonString = JSON.stringify(inputData);
+const urlEncodedString = encodeURIComponent(jsonString);
+
 export default function Home() {
+  const [ATOM, setATOM] = useState<any[]>([]);
+  const [NTRN, setNTRN] = useState<any[]>([]);
+  const [minOrMaxATOMValue, setMinOrMaxATOMValue] = useState<number[]>([]);
+  const [minOrMaxNTRNValue, setMinOrMaxNTRNValue] = useState<number[]>([]);
+  const [avgValue, setAvgValue] = useState<number[]>([]);
+  const [dates, setDates] = useState<any[]>([]);
+
+  const getDates = (series: any[]): number[] => series.map((item) => item.time);
+  const getY = (series: any[]): number[] => series.map((item) => item.value);
+
+  const getAvgValue = (series: any[]): number => {
+    if (series.length === 0) {
+      return 0;
+    }
+    const sum = series.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    const average = sum / series.length;
+    return Number(average.toFixed(2));
+  };
+
+  const getReadableDate = (item: number) => {
+    const date = new Date(item * 1000);
+    const formattedTime = date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    return formattedTime;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://app.astroport.fi/api/trpc/charts.prices?input=${urlEncodedString}`
+        );
+        const {
+          result: {
+            data: { json },
+          },
+        } = await response.json();
+        const ibc =
+          json[
+            "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
+          ];
+        const untrn = json["untrn"];
+        setDates(getDates(ibc.series));
+        setATOM(getY(ibc.series));
+        setNTRN(getY(untrn.series));
+        setMinOrMaxATOMValue([ibc.minValue, ibc.maxValue]);
+        setMinOrMaxNTRNValue([untrn.minValue, untrn.maxValue]);
+        setAvgValue([
+          getAvgValue(getY(ibc.series)),
+          getAvgValue(getY(untrn.series)),
+        ]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <ResponsiveChartContainer
+        xAxis={[
+          {
+            data: dates,
+            id: "x-axis-id",
+            scaleType: "time",
+            valueFormatter: getReadableDate,
+            label: "Time",
+          },
+          { labelFontSize: 50 },
+        ]}
+        yAxis={[{ label: "Price" }]}
+        series={[
+          { type: "line", label: `ATOM (Average:${avgValue[0]})`, data: ATOM },
+          { type: "line", label: `NTRN (Average:${avgValue[1]})`, data: NTRN },
+        ]}
+        height={600}
+        margin={{
+          left: 96,
+          right: 96,
+        }}
+      >
+        <LinePlot />
+        <ChartsXAxis />
+        <ChartsYAxis />
+        <MarkPlot />
+        <ChartsLegend />
+        <LineHighlightPlot />
+        <ChartsAxisHighlight x="line" />
+        <ChartsTooltip trigger="axis" />
+        <ChartsReferenceLine
+          y={minOrMaxATOMValue[0] || 0}
+          label={`ATOM Min: ${minOrMaxATOMValue[0]}`}
+          labelAlign="end"
+          lineStyle={{ stroke: "#128128", strokeDasharray: "3 3" }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <ChartsReferenceLine
+          y={minOrMaxATOMValue[1] || 0}
+          label={`ATOM Max: ${minOrMaxATOMValue[1]}`}
+          labelAlign="end"
+          lineStyle={{ stroke: "#128128", strokeDasharray: "3 3" }}
+        />
+        <ChartsReferenceLine
+          y={minOrMaxNTRNValue[0] || 0}
+          spacing={-14}
+          label={`NTRN Min: ${minOrMaxNTRNValue[0]}`}
+          labelAlign="end"
+          lineStyle={{ stroke: "#998909", strokeDasharray: "3 3" }}
+        />
+        <ChartsReferenceLine
+          y={minOrMaxNTRNValue[1] || 0}
+          label={`NTRN Max: ${minOrMaxNTRNValue[1]}`}
+          labelAlign="end"
+          lineStyle={{ stroke: "#998909", strokeDasharray: "3 3" }}
+        />
+      </ResponsiveChartContainer>
     </main>
   );
 }
